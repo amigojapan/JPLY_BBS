b93292b24a96ce52d9051f3087505fa3ff8f0944
Usmar A Padow <usmpadow@gmail.com>
2026-03-20 13:37:58 +0900

    latets update

--- scripts/test.js
+++ scripts/test.js
@@ -5,14 +5,14 @@
             art: "test"
         };
 
-        let result = await CURL(
-            "server_side/ascii_post.php",
-            postData
-        );
+        //let result = await CURL(
+        //    "server_side/ascii_post.php",
+        //    postData
+        //);
 
-        var response = await CURL('https://amjp.psy-k.org/JPLY_BBS/server_side/ascii_post.php', {
-            data: 'NICK="test"&PASSWORD="test"&title="test"+&art="test"'
+        var result = await CURL('https://amjp.psy-k.org/JPLY_BBS/server_side/ascii_post.php', {
+            data: 'NICK=test&PASSWORD=test&title=test+&art=test'
         });
 
     PRINT("===== DEBUG RESULT =====");
-    PRINT(JSON.stringify(result));
+    PRINT(result);
